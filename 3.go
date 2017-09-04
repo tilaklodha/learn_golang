40a999e7d464019613924a7a0cc8db395d6e8799
tilak.l <tilak.l@go-jek.com>
2017-09-04 20:29:15 +0530

    Added 3.go

---
+++ 3.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// First way to define variable
+	var power int
+	power = 9000
+
+	// Second way to define variable
+	power1 := 9000
+
+	fmt.Println("It's over", power, power1)
+}
